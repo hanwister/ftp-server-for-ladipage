c424500d4ee6826fd772f6833fb37986f8fdc631
hanwister <54110906+hanwister@users.noreply.github.com>
2025-06-18 06:38:48 +0700

    Start-serve

---
+++ index.js
@@ -0,0 +1,47 @@
+import FtpSrv from "ftp-srv";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Địa chỉ và port server FTP
+const ftpServer = new FtpSrv({
+  url: "ftp://0.0.0.0:21",
+  pasv_url: "127.0.0.1", // dùng IP thật nếu dùng public
+  anonymous: false
+});
+
+console.log(__dirname)
+
+// Thư mục lưu file
+const ROOT_DIR = path.resolve(__dirname, 'uploads');
+
+// Tạo thư mục nếu chưa tồn tại
+if (!fs.existsSync(ROOT_DIR)) {
+  fs.mkdirSync(ROOT_DIR);
+}
+
+// Xác thực user đăng nhập
+ftpServer.on('login', ({ username, password }, resolve, reject) => {
+  if (username === 'user' && password === 'pass') {
+    resolve({ root: ROOT_DIR });
+  } else {
+    reject(new Error('Sai thông tin đăng nhập'));
+  }
+});
+
+// Log khi có kết nối
+ftpServer.on('client-connected', (client) => {
+  console.log('Client connected:', client.ip);
+});
+
+ftpServer.listen()
+  .then(() => {
+    console.log('FTP server đang chạy tại ftp://localhost:21');
+    console.log('Đăng nhập với user: user / pass: pass');
+  })
+  .catch(err => {
+    console.error('Lỗi khởi động server:', err);
+  });
